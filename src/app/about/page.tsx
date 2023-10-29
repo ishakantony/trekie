889a2c7f36e83312432e324493c02dc4e5e675a7
stephanier0 <louisastephanie092@gmail.com>
2023-10-29 18:13:03 +0800

    feat(#10): about us page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,80 @@
+
+export default function Page() {
+  return (
+    <div
+      className="mx-auto mb-20 mt-20 flex max-w-screen-sm flex-col items-center 
+    justify-center p-10 text-lg font-medium"
+    >
+      <h1 className="text-4xl">About Trekie</h1>
+      <p className="mt-5 text-justify">
+        At Trekie, we are dedicated to simplifying leave management for
+        organizations of all sizes. Our mission is to streamline the process of
+        recording, tracking, and managing employee leaves, empowering businesses
+        to focus on what matters most – their people and their growth.
+      </p>
+
+      <h1 className="mt-10 text-4xl">Who We Are</h1>
+      <p className="mt-5 text-justify">
+        We are a team of enthusiastic professionals committed to creating
+        user-friendly solutions that transform the way companies handle employee
+        leave management. With a deep understanding of the complexities and
+        challenges faced by HR departments, we've developed an application that
+        optimizes leave tracking and administration, so you can allocate more
+        time to strategic HR tasks.
+      </p>
+
+      <h1 className="mt-10 text-4xl">What We Do</h1>
+      <p className="mt-5 text-justify">
+        Our innovative employee leave application is designed to make leave
+        management effortless. From requesting and approving leave to monitoring
+        accruals and generating reports, our platform centralizes all
+        leave-related tasks. It provides your HR team, managers, and employees
+        with a simple, intuitive tool to ensure leave management is both
+        efficient and transparent.
+      </p>
+
+      <h1 className="mt-10 text-4xl">Why Choose Us</h1>
+      <p className="mt-5 text-justify">
+        <ul className="ml-6 list-disc">
+          <li>
+            Simplicity and Accessibility: Our user-friendly application is
+            designed for ease of use, ensuring that employees and HR teams can
+            manage leave with minimum effort.
+          </li>
+          <li>
+            Compliance and Accuracy: Stay compliant with labor laws and company
+            policies while maintaining precise records of leave balances and
+            accruals.
+          </li>
+          <li>
+            Efficient Workflow: Streamline the leave approval process, reducing
+            the administrative burden on HR staff.
+          </li>
+          <li>
+            Transparency and Communication: Enhance transparency and facilitate
+            communication between employees and HR, resulting in a more
+            harmonious workplace.
+          </li>
+          <li>
+            Real-Time Insights: Leverage accurate data to make informed
+            decisions about workforce planning and resource allocation.
+          </li>
+        </ul>
+      </p>
+
+      <h1 className="mt-10 text-4xl">Join Us on the Journey</h1>
+      <p className="mt-5 text-justify">
+        We invite you to join us in reimagining leave management. At Trekie,
+        we're not just offering software; we're offering a partnership that will
+        help you optimize your HR processes and create a more engaged and
+        productive workforce. Let's embark on this journey to revolutionize how
+        you manage employee leaves.
+      </p>
+
+      <p className="mt-10 text-justify">
+        Contact Us to learn more about our employee leave management application
+        and how we can simplify your HR operations.
+      </p>
+    </div>
+  );
+}
